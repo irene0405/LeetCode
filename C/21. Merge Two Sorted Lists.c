e765ed7e082fa08a1e88401b0d6af92e7912374b
irene0405 <91746403+irene0405@users.noreply.github.com>
2022-10-28 16:20:35 +0800

    Update 21. Merge Two Sorted Lists.c

--- C/21. Merge Two Sorted Lists.c
+++ C/21. Merge Two Sorted Lists.c
@@ -7,39 +7,39 @@
  */
 
 
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    struct ListNode*l1 = list1;
-    struct ListNode*l2 = list2;
-    if (!l1){
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2) {
+    struct ListNode *l1 = list1;
+    struct ListNode *l2 = list2;
+    if (!l1) {
         return l2;
     }
-    if (!l2){
+    if (!l2) {
         return l1;
     }
     struct ListNode *p, *list;
-    if (l1->val <= l2->val){
+    if (l1->val <= l2->val) {
         p = l1;
         l1 = l1->next;
-    }else{
+    } else {
         p = l2;
         l2 = l2->next;
     }
     list = p;
-    while (l1 && l2){
-        if (l1->val < l2-> val){
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
             p->next = l1;
             l1 = l1->next;
-        }else{
+        } else {
             p->next = l2;
             l2 = l2->next;
         }
         p->next->next = NULL;
         p = p->next;
     }
-    if (!l1){
+    if (!l1) {
         p->next = l2;
     }
-    if (!l2){
+    if (!l2) {
         p->next = l1;
     }
     return list;
