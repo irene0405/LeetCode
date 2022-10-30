732ff879a4147ad9a55bf1bd161300ac98a7c6d0
irene0405 <91746403+irene0405@users.noreply.github.com>
2022-10-30 22:35:19 +0800

    Update 110. Balanced Binary Tree_1.cpp

--- C++/110. Balanced Binary Tree_1.cpp
+++ C++/110. Balanced Binary Tree_1.cpp
@@ -1,27 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isBalanced(TreeNode *root) {
-        if (!root) return true;
+        if (!root) {
+            return true;
+        }
         bool balanced = true;
-        height(root, &balanced);
+        getHeight(root, &balanced);
         return balanced;
     }
 
 private:
-    int height(TreeNode *root, bool *balanced) {
-        if (!root) return 0;
-        int left_height = height(root->left, balanced);
-        if (!balanced) return false;
-        int right_height = height(root->right, balanced);
-        if (!balanced) return false;
-        if (abs(left_height - right_height) > 1) {
+    int getHeight(TreeNode *root, bool *balanced) {
+        if (!root) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(root->left, balanced);
+        if (!balanced) {
+            return false;
+        }
+        int rightHeight = getHeight(root->right, balanced);
+        if (!balanced) {
+            return false;
+        }
+        if (abs(leftHeight - rightHeight) > 1) {
             *balanced = false;
             return false;
         }
-        return max(left_height, right_height) + 1;
+        return max(leftHeight, rightHeight) + 1;
     }
 };
 
-// Runtime: 35 ms, faster than 18.57% of C++ online submissions for Balanced Binary Tree.
+// Runtime: 26 ms, faster than 51.31% of C++ online submissions for Balanced Binary Tree.
 // Memory Usage: 20.9 MB, less than 80.96% of C++ online submissions for Balanced Binary Tree.
 
