2d2bcfaaa816fd74092e49ce6b7deef37aa46bf1
irene0405 <91746403+irene0405@users.noreply.github.com>
2022-10-30 23:56:39 +0800

    Create 232. Implement Queue using Stacks.cpp

---
+++ C++/232. Implement Queue using Stacks.cpp
@@ -0,0 +1,54 @@
+class MyQueue {
+public:
+    MyQueue() {
+
+    }
+
+    void push(int x) {
+        s1.push(x);
+    }
+
+    int pop() {
+        if (s2.empty()) {
+            move();
+        }
+        int top = s2.top();
+        s2.pop();
+        return top;
+    }
+
+    int peek() {
+        if (s2.empty()) {
+            move();
+        }
+        return s2.top();
+    }
+
+    bool empty() {
+        return (s1.empty() && s2.empty());
+    }
+
+private:
+    stack<int> s1;
+    stack<int> s2;
+
+    void move() {
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+ 
+// Runtime: 7 ms, faster than 10.97% of C++ online submissions for Implement Queue using Stacks.
+// Memory Usage: 7.1 MB, less than 12.83% of C++ online submissions for Implement Queue using Stacks.
+
