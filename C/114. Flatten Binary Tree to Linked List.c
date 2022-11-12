7d657a5362a4f48b4620a5fb165bd004f6804a79
irene0405 <91746403+irene0405@users.noreply.github.com>
2022-11-12 17:13:17 +0800

    Update 114. Flatten Binary Tree to Linked List.c

--- C/114. Flatten Binary Tree to Linked List.c
+++ C/114. Flatten Binary Tree to Linked List.c
@@ -12,14 +12,24 @@
 class Solution{
         public:
         void flatten(TreeNode* root) {
-            if (!root) return;
-            if (root->left) flatten(root->left);
-            if (root->right) flatten(root->right);
-            TreeNode *tmp = root->right;
+            if (root == NULL) {
+                return;
+            }
+
+            if (root->left != NULL) {
+                flatten(root->left);
+            }
+            if (root->right != NULL) {
+                flatten(root->right);
+            }
+
+            struct TreeNode *temp = root->right;
             root->right = root->left;
             root->left = NULL;
-            while (root->right) root = root->right;
-            root->right = tmp;
+            while (root->right != NULL) {
+                root = root->right;
+            }
+            root->right = temp;
         }
 };
 
